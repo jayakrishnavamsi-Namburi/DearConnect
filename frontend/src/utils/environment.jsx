00c6ea2037a361d519cc79a2c35d47b4bad513ee
jayakrishnavamsi-Namburi <jayakrishnavamsi.n@gmail.com>
2025-07-29 07:24:38 +0530

    deploy frontend

--- frontend/src/utils/environment.jsx
+++ frontend/src/utils/environment.jsx
@@ -1,7 +1,7 @@
 let IS_PROD = false;
 
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"  // your deployed backend
+  ? "https://dearconnectbackend.onrender.com"  // your deployed backend
   : "http://localhost:5000";                   // dev backend
 
 export default server;
