a30362f784250fb3cd9915d901efd276ecf25f45
jayakrishnavamsi-Namburi <jayakrishnavamsi.n@gmail.com>
2025-08-04 00:27:47 +0530

    Auth

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,9 +1,6 @@
-// let IS_PROD = true;
-const server = "http://localhost:8000"
+let IS_PROD = true;
+const server = IS_PROD ? "https://dearconnect-backend.onrender.com" : "http://localhost:8000";
 
 
 export default server;
 
-
-// IS_PROD ?
-//     "https://apnacollegebackend.onrender.com" :
